b7da87ce39a36fb23780183ed4611e42635eb795
Connor Mallon <connor.mallon@qut.edu.au>
2025-11-12 08:12:23 +1000

    self adjoint getting the right gradient except sign

--- src/InverseTest.jl
+++ src/InverseTest.jl
@@ -31,23 +31,25 @@ dΩ = Measure(Ω,2*order)
 ## Spaces
 reffe_scalar = ReferenceFE(lagrangian,Float64,order)
 V = TestFESpace(model,reffe_scalar;dirichlet_tags=[1,2,3,4,5,6,7])
-U = TrialFESpace(V,1.0)
+U = TrialFESpace(V,0.0)
 V_φ = TestFESpace(model,reffe_scalar;dirichlet_tags=["boundary"])
 
 f(x) = 1.0
 a(u,v,p) = ∫(p*∇(u)⋅∇(v))dΩ
 l(v,p) = ∫(f⋅v)dΩ
+
 state_map = AffineFEStateMap(a,l,U,V,V_φ)
 
 res(u,v,p) = a(u,v,p) - l(v,p)
-u_data(x) = sin(pi*x[1])*sin(pi*x[2])
-α= 1
 
-misfit(u) = (u-u_data)^2
+#u_data(x) = sin(pi*x[1])*sin(pi*x[2])
+#α= 1
+
+#misfit(u) = (u-u_da ta)^2
 
 #J(u,p) = ∫( (u-u_data)*(u-u_data) + 0*p*p )dΩ # keep p term otherwise dual error
 
-J(u,p) = ∫( u*u*p*p  )dΩ # keep p term otherwise dual error
+J(u,p) = ∫( f*u + 0*p )dΩ # keep p term otherwise dual error
 
 objective = GridapTopOpt.StateParamMap(J,state_map)
 
@@ -56,20 +58,62 @@ function p_to_j(p)
     j = objective(u,p)
 end
 
-p = interpolate(0.5,V_φ).free_values
+p = interpolate(20,V_φ).free_values
 u = copy(state_map(p))
 
 using Zygote
 
 djdp = Zygote.gradient(p_to_j, p)[1]
 λ = state_map.cache.adj_cache[3]
-ṗ = djdp # take as the first guess for the direction
+ṗ = djdp * 1e4# take as the first guess for the direction
 
 uh = FEFunction(U,u)
 ph = FEFunction(V_φ,p)
 φh = ph 
 λh = FEFunction(V,λ)
 ṗh = FEFunction(V_φ,ṗ)
+
+function jacobian_fdm_4th(func, x::AbstractVector; h = 1e-4)
+    # Work on a floating-point copy of x
+    x_work = float.(copy(x))
+
+    y0 = func(x_work)
+    y0 isa AbstractVector || error("jacobian_fdm_4th: func(x) must return a vector, got $(typeof(y0))")
+
+    m = length(y0)
+    n = length(x_work)
+
+    T = promote_type(eltype(x_work), eltype(y0))
+    J = Matrix{T}(undef, m, n)
+
+    for j in 1:n
+        xj = x_work[j]
+
+        # f(x + 2h e_j)
+        x_work[j] = xj + 2h
+        f_pp = copy(func(x_work))
+
+        # f(x + h e_j)
+        x_work[j] = xj + h
+        f_p = copy(func(x_work))
+
+        # f(x - h e_j)
+        x_work[j] = xj - h
+        f_m = copy(func(x_work))
+
+        # f(x - 2h e_j)
+        x_work[j] = xj - 2h
+        f_mm = copy(func(x_work))
+
+        # restore x
+        x_work[j] = xj
+
+        @inbounds J[:, j] .= (-f_pp .+ 8f_p .- 8f_m .+ f_mm) ./ (12h)
+    end
+
+    return J
+end
+
 
 ######
 ###### incremental equation (ṗ-u̇)
@@ -133,38 +177,56 @@ end
 ∂2J∂φ∂u_mat = assemble_matrix(∂2J∂φ∂u,U,V_φ)
 ∂2J∂φ∂u_mat_u̇ = ∂2J∂φ∂u_mat * u̇
 
-## Testing
-# ∂²J / ∂u² * u̇
-dv = get_fe_basis(V)
-du = get_trial_fe_basis(U)
-dφ = get_fe_basis(V_φ)
-dφ_ = get_trial_fe_basis(V_φ)
-
-∂2∂u2_analytical(uh) = ∫( 2*φh*φh*du⋅dv )dΩ
-∂2∂u2_matrix_analytical = assemble_matrix(∂2∂u2_analytical(uh),U,U)
-@test ∂2∂u2_matrix_analytical ≈ ∂2J∂u2_mat
-@test ∂2∂u2_matrix_analytical * u̇ ≈ ∂2J∂u2_mat_u̇
-
-# ∂/∂p (∂J/∂u ) * ṗ
-∂2J∂u∂φ_analytical(uh,φh) = ∫( 4*φh*uh*dφ_⋅dv )dΩ
-∂2J∂u∂φ_matrix_analytical = assemble_matrix(∂2J∂u∂φ_analytical(uh,φh),V_φ,U)
-@test ∂2J∂u∂φ_matrix_analytical ≈ ∂2J∂u∂φ_mat
-@test ∂2J∂u∂φ_matrix_analytical * ṗ ≈ ∂2J∂u∂φ_mat_ṗ
-
-# ∂²J / ∂p² * ṗ
-∂2J∂φ2_analytical(uh) = ∫( 2*uh*uh*dφ⋅dφ_ )dΩ
-∂2J∂φ2_matrix_analytical = assemble_matrix(∂2J∂φ2_analytical(uh),V_φ,V_φ)
-@test ∂2J∂φ2_matrix_analytical ≈ ∂2J∂φ2_mat
-@test ∂2J∂φ2_matrix_analytical * ṗ ≈ ∂2J∂φ2_mat_ṗ
-
-# ∂/∂u (∂J / ∂p) * u̇
-∂2J∂φ∂u_analytical(uh,φh) = ∫( 4*uh*φh*du⋅dφ )dΩ
-∂2J∂φ∂u_matrix_analytical = assemble_matrix(∂2J∂φ∂u_analytical(uh,φh),U,V_φ)
-@test ∂2J∂φ∂u_matrix_analytical ≈ ∂2J∂φ∂u_mat
-@test ∂2J∂φ∂u_matrix_analytical * u̇ ≈ ∂2J∂φ∂u_mat_u̇
-
 dṗ = ∂2J∂φ2_mat_ṗ + ∂2J∂φ∂u_mat_u̇
 du̇ = ∂2J∂u2_mat_u̇ + ∂2J∂u∂φ_mat_ṗ
+
+# ## Testing
+# # ∂²J / ∂u² * u̇
+# dv = get_fe_basis(V)
+# du = get_trial_fe_basis(U)
+# dφ = get_fe_basis(V_φ)
+# dφ_ = get_trial_fe_basis(V_φ)
+
+# ∂2∂u2_analytical(uh) = ∫( 2*φh*φh*du⋅dv )dΩ
+# ∂2∂u2_matrix_analytical = assemble_matrix(∂2∂u2_analytical(uh),U,U)
+# @test ∂2∂u2_matrix_analytical ≈ ∂2J∂u2_mat
+# @test ∂2∂u2_matrix_analytical * u̇ ≈ ∂2J∂u2_mat_u̇
+
+# # ∂/∂p (∂J/∂u ) * ṗ
+# ∂2J∂u∂φ_analytical(uh,φh) = ∫( 4*φh*uh*dφ_⋅dv )dΩ
+# ∂2J∂u∂φ_matrix_analytical = assemble_matrix(∂2J∂u∂φ_analytical(uh,φh),V_φ,U)
+# @test ∂2J∂u∂φ_matrix_analytical ≈ ∂2J∂u∂φ_mat
+# @test ∂2J∂u∂φ_matrix_analytical * ṗ ≈ ∂2J∂u∂φ_mat_ṗ
+
+# # ∂²J / ∂p² * ṗ
+# ∂2J∂φ2_analytical(uh) = ∫( 2*uh*uh*dφ⋅dφ_ )dΩ
+# ∂2J∂φ2_matrix_analytical = assemble_matrix(∂2J∂φ2_analytical(uh),V_φ,V_φ)
+# @test ∂2J∂φ2_matrix_analytical ≈ ∂2J∂φ2_mat
+# @test ∂2J∂φ2_matrix_analytical * ṗ ≈ ∂2J∂φ2_mat_ṗ
+
+# # ∂/∂u (∂J / ∂p) * u̇
+# ∂2J∂φ∂u_analytical(uh,φh) = ∫( 4*uh*φh*du⋅dφ )dΩ
+# ∂2J∂φ∂u_matrix_analytical = assemble_matrix(∂2J∂φ∂u_analytical(uh,φh),U,V_φ)
+# @test ∂2J∂φ∂u_matrix_analytical ≈ ∂2J∂φ∂u_mat
+# @test ∂2J∂φ∂u_matrix_analytical * u̇ ≈ ∂2J∂φ∂u_mat_u̇
+
+dj = 1.0
+function dj_to_du(u)
+    j_val, j_pullback = rrule(objective,u,φ)   # Compute functional and pull back
+    _, dFdu, dFdφ     = j_pullback(dj)    # Compute dFdu, dFdφ
+    dFdu,dFdφ
+    dFdu
+end
+dJ̇ = 1.0
+@test jacobian_fdm_4th( dj_to_du, u) * u̇ * dJ̇  ≈ du̇ atol = 1e-6
+
+function dj_to_dφ(φ)
+    j_val, j_pullback = rrule(objective,u,φ)   # Compute functional and pull back
+    _, dFdu, dFdφ     = j_pullback(dj)    # Compute dFdu, dFdφ
+    dFdφ
+end
+dJ̇ = 1.0
+@test jacobian_fdm_4th( dj_to_dφ, p) * ṗ * dJ̇ ≈ dṗ
 
 ######
 ###### partials related to the state map
@@ -194,30 +256,30 @@ du̇ = ∂2J∂u2_mat_u̇ + ∂2J∂u∂φ_mat_ṗ
 ∂2R∂φ∂u_mat = assemble_matrix(∂2R∂φ∂u,U,V_φ)
 ∂2R∂φ∂u_mat_u̇ = ∂2R∂φ∂u_mat * u̇
 
-## TESTING
-# ∂²R / ∂u² * u̇ * λ
-∂2∂u2R_analytical(uh,λh,φh) = ∫( 0*du*dv )dΩ
-∂2∂u2R_matrix_analytical = assemble_matrix(∂2∂u2R_analytical(uh,λh,φh),U,U)
-@test ∂2∂u2R_matrix_analytical ≈ ∂2R∂u2_mat
-@test ∂2∂u2R_matrix_analytical * u̇ ≈ ∂2R∂u2_mat_u̇
+# ## TESTING
+# # ∂²R / ∂u² * u̇ * λ
+# ∂2∂u2R_analytical(uh,λh,φh) = ∫( 0*du*dv )dΩ
+# ∂2∂u2R_matrix_analytical = assemble_matrix(∂2∂u2R_analytical(uh,λh,φh),U,U)
+# @test ∂2∂u2R_matrix_analytical ≈ ∂2R∂u2_mat
+# @test ∂2∂u2R_matrix_analytical * u̇ ≈ ∂2R∂u2_mat_u̇
 
-# ∂/∂p (∂R/∂u * λ) * ṗ
-∂2R∂u∂φ_analytical(uh,λh,φh) = ∫( dφ_* ∇(dv) ⋅ ∇(λh)  )dΩ
-∂2R∂u∂φ_matrix_analytical = assemble_matrix(∂2R∂u∂φ_analytical(uh,λh,φh),V_φ,U)
-@test ∂2R∂u∂φ_matrix_analytical ≈ ∂2R∂u∂φ_mat
-@test ∂2R∂u∂φ_matrix_analytical * ṗ ≈ ∂2R∂u∂φ_mat_ṗ
+# # ∂/∂p (∂R/∂u * λ) * ṗ
+# ∂2R∂u∂φ_analytical(uh,λh,φh) = ∫( dφ_* ∇(dv) ⋅ ∇(λh)  )dΩ
+# ∂2R∂u∂φ_matrix_analytical = assemble_matrix(∂2R∂u∂φ_analytical(uh,λh,φh),V_φ,U)
+# @test ∂2R∂u∂φ_matrix_analytical ≈ ∂2R∂u∂φ_mat
+# @test ∂2R∂u∂φ_matrix_analytical * ṗ ≈ ∂2R∂u∂φ_mat_ṗ
 
-# ∂²R / ∂p² * ṗ * λ
-∂2R∂φ2_analytical(uh,λh) = ∫( 0*dφ⋅dφ_ )dΩ
-∂2R∂φ2_matrix_analytical = assemble_matrix(∂2R∂φ2_analytical(uh,λh),V_φ,V_φ)
-@test ∂2R∂φ2_matrix_analytical ≈ ∂2R∂φ2_mat
-@test ∂2R∂φ2_matrix_analytical * ṗ ≈ ∂2R∂φ2_mat_ṗ   
+# # ∂²R / ∂p² * ṗ * λ
+# ∂2R∂φ2_analytical(uh,λh) = ∫( 0*dφ⋅dφ_ )dΩ
+# ∂2R∂φ2_matrix_analytical = assemble_matrix(∂2R∂φ2_analytical(uh,λh),V_φ,V_φ)
+# @test ∂2R∂φ2_matrix_analytical ≈ ∂2R∂φ2_mat
+# @test ∂2R∂φ2_matrix_analytical * ṗ ≈ ∂2R∂φ2_mat_ṗ   
 
-# ∂/∂u (∂R/∂p * λ) * ṗ
-∂2R∂φ∂u_analytical(uh,λh,φh) = ∫( dφ * ∇(du) ⋅ ∇(λh) )dΩ   
-∂2R∂φ∂u_matrix_analytical = assemble_matrix(∂2R∂φ∂u_analytical(uh,λh,φh),U,V_φ)
-@test ∂2R∂φ∂u_matrix_analytical ≈ ∂2R∂φ∂u_mat
-@test ∂2R∂φ∂u_matrix_analytical * u̇ ≈ ∂2R∂φ∂u_mat_u̇
+# # ∂/∂u (∂R/∂p * λ) * ṗ
+# ∂2R∂φ∂u_analytical(uh,λh,φh) = ∫( dφ * ∇(du) ⋅ ∇(λh) )dΩ   
+# ∂2R∂φ∂u_matrix_analytical = assemble_matrix(∂2R∂φ∂u_analytical(uh,λh,φh),U,V_φ)
+# @test ∂2R∂φ∂u_matrix_analytical ≈ ∂2R∂φ∂u_mat
+# @test ∂2R∂φ∂u_matrix_analytical * u̇ ≈ ∂2R∂φ∂u_mat_u̇
 
 ######
 ###### incremental adjoint equation
@@ -226,17 +288,29 @@ du̇ = ∂2J∂u2_mat_u̇ + ∂2J∂u∂φ_mat_ṗ
 # RHS 
 inc_adjoint_rhs = - ∂2J∂u2_mat_u̇ - ∂2J∂u∂φ_mat_ṗ - ∂2R∂u2_mat_u̇ - ∂2R∂u∂φ_mat_ṗ
 
-# LHS 2
+
+
+
+
+
+# LHS 2w
 assem_adjoint = SparseMatrixAssembler(V,U)
-∂R∂u_adjoint= (du,v) -> Gridap.jacobian(res,[uh,v,φh],1)
+∂R∂u_adjoint = (du,v) -> Gridap.jacobian(res,[uh,v,φh],1)
 ∂R∂u_adjoint_mat = assemble_adjoint_matrix(∂R∂u_adjoint,assem_adjoint,U,V)
 
 λ⁻ = ∂R∂u_adjoint_mat \ inc_adjoint_rhs
+
+λ⁻ 
+u̇
+
+
+
+
 λ⁻h = FEFunction(V,λ⁻)
 ∂R∂p_λ⁻ = Gridap.gradient(φh->res(uh,λ⁻h,φh),φh)
 ∂R∂p_mat_λ⁻ = assemble_vector(∂R∂p_λ⁻,V_φ)
 
-dṗ_adj = ∂R∂p_mat_λ⁻ - ∂2R∂φ2_mat_ṗ - ∂2R∂φ∂u_mat_u̇
+dṗ_adj = ∂R∂p_mat_λ⁻ + ∂2R∂φ2_mat_ṗ + ∂2R∂φ∂u_mat_u̇
 
 #### testing
 
@@ -251,6 +325,30 @@ dṗ_adj_fd = dφdu_fd' * du̇
 
 @test dṗ_adj[1] ≈ dṗ_adj_fd
 
+
+
+
+
+function dj_to_dφ_adj(φ)
+    u, u_pullback = rrule(state_map,φ)
+    j_val, j_pullback = rrule(objective,u,φ)   # Compute functional and pull back
+    _, dFdu, dFdφ     = j_pullback(dj)    # Compute dFdu, dFdφ
+    _, dφ_adj         = u_pullback(dFdu) # Compute -dFdu*dudφ via adjoint
+    dφ_adj 
+end
+
+dj_to_dφ_adj(φ)
+jacobian_fdm_4th( dj_to_dφ_adj, p)* ṗ
+
+
+
+
+# self adjoint problem? 
+
+
+
+
+
 ###### 
 
 # Finally, the hessian action can then be computed as:
@@ -261,24 +359,48 @@ dṗ_adj_fd = dφdu_fd' * du̇
 ∂2R∂φ2_mat_ṗ
 ∂2R∂φ∂u_mat_u̇
 
-Hṗ = ∂2J∂φ2_mat_ṗ + ∂2J∂φ∂u_mat_u̇ + dṗ_adj
-
-
-
-
-
-#end
-
-#Hṗ = hessian_action(u,p,λ,ṗ)
-
+Hṗ = dṗ + dṗ_adj
 
 # ive tested all the partials.... 
+
+
+
+
+
+
+function p_to_dp(φ)
+    u, u_pullback = rrule(state_map,φ)
+    j_val, j_pullback = rrule(objective,u,φ)   # Compute functional and pull back
+    _, dFdu, dFdφ     = j_pullback(1)    # Compute dFdu, dFdφ
+    _, dφ_adj         = u_pullback(dFdu) # Compute -dFdu*dudφ via adjoint
+    dφ_adj + dFdφ
+end
+
+grad(central_fdm(5,1),p_to_dp,p)
+jacobian_fdm_4th(p_to_dp, p)* ṗ
+
+
+
+
+
+
+
+j_val, j_pullback = rrule(objective,uh,φh)   # Compute functional and pull back
+
+function dj_to_du(dj)
+    _, dFdu, dFdφ     = j_pullback(dj)    # Compute dFdu, dFdφ
+    dFdu,dFdφ
+    dFdu
+end
+
+dj_to_du(1.0)
+central_fdm(5,1)(dj_to_du,1.0)
+
+
 
 H_fd = central_fdm(5,1)(p->Zygote.gradient(p_to_j,[p])[1][1],p[1])
 Hṗ_fd = H_fd * ṗ
 @test Hṗ ≈ Hṗ_fd 
-
-
 
 
 
